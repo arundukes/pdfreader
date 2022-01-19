e691689ede753f31726abd1fa92d001adb421c44
arunvs909 <50038763+arundukes@users.noreply.github.com>
2022-01-19 15:09:04 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/paradox/pdfreader/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.paradox.pdfreader;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.paradox.pdfreader";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "16.0";
+}
